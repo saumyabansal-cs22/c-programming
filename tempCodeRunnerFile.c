7001596c7c604c4c0c6cde5c7e68eface7902172
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-02-18 22:53:54 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+// int main(){
+//     char str[100];
+//     scanf("%[^\n]s",str);
+//     int i=0;
+//     while (str[i]!='\0'){
+
+//         i++;
+//     }
+//     printf("length of the string=%d",i);
+// }
